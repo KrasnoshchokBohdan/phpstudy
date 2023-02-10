35431da64e3dfeab1caddddcf7dd6312bc7802ee
DanK894 <82168352+DanK894@users.noreply.github.com>
2023-02-10 18:17:46 +0200

    2

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo 'Hello Amigos !!!';
+declare(strict_types=1);
+
+use \Phpstudy\Example\Amigos;
+
+require __DIR__.'/vendor/autoload.php';
+
+new Amigos();
+
---
+++ src/Example/Amigos.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phpstudy\Example;
+
+class Amigos
+{
+    public function __construct()
+    {
+        echo 'Hello Amigos !!!';
+    }
+
+}
+
